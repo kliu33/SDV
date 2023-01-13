ca621d78fd1624f28cec6a7ed1b84224fe5ba0fb
kliu33 <kliu33@buffalo.edu>
2023-01-13 16:49:39 -0500

    added css styling to main page

--- src/canvas.js
+++ src/canvas.js
@@ -6,40 +6,40 @@ document.addEventListener("DOMContentLoaded", function () {
   const ctx = canvas.getContext("2d");
   const tWidth = 40;
   const tHeight = 40;
-  const rows = 15;
-  const cols = 20;
+  const rows = 20;
+  const cols = 15;
   
 
 
   let actions = [];
-  const char_up_right = new Image(40,40);
+  const char_up_right = new Image(tWidth,tWidth);
   char_up_right.src = "./images/char_up_right.png"
-  const char_up_left = new Image(40,40);
+  const char_up_left = new Image(tWidth,tWidth);
   char_up_left.src = "./images/char_up_left.png"
-  const char_down_left = new Image(40,40);
+  const char_down_left = new Image(tWidth,tWidth);
   char_down_left.src = "./images/char_down_left.png"
-  const char_down_right = new Image(40,40);
+  const char_down_right = new Image(tWidth,tWidth);
   char_down_right.src = "./images/char_down_right.png"
 
-  const char_left = new Image(40,40);
+  const char_left = new Image(tWidth,tWidth);
   char_left.src = "./images/char_left.png"
-  const char_right = new Image(40,40);
+  const char_right = new Image(tWidth,tWidth);
   char_right.src = "./images/char_right.png"
-  const char_up = new Image(40,40);
+  const char_up = new Image(tWidth,tWidth);
   char_up.src = "./images/char_up.png"
-  const char_down = new Image(40,40);
+  const char_down = new Image(tWidth,tWidth);
   char_down.src = "./images/char_down.png"
-  const wood = new Image(40,40);
+  const wood = new Image(tWidth,tWidth);
   wood.src = "./images/wood.jpg"
-  const water = new Image(40,40);
+  const water = new Image(tWidth,tWidth);
   water.src = "./images/water.jpg"
-  const brick = new Image(40,40);
+  const brick = new Image(tWidth,tWidth);
   brick.src = "./images/brick.jpg"
-  const grass = new Image(40,40);
+  const grass = new Image(tWidth,tWidth);
   grass.src = "./images/grass.png"
-  const rock = new Image(40,40);
+  const rock = new Image(tWidth,tWidth);
   rock.src = "./images/rock.jpg"
-  const grass_water = new Image(40,40);
+  const grass_water = new Image(tWidth,tWidth);
   grass_water.src = "./images/grass_water.jpg"
     let facing="down";
     let xpos = 10;
@@ -153,57 +153,57 @@ document.addEventListener("DOMContentLoaded", function () {
       for (let j = 0; j < cols; j++) {
         let idx = i * cols + j;
         if(map[idx] === 1) {
-          ctx.drawImage(rock, tWidth * j, tHeight * i, 40, 40);
+          ctx.drawImage(rock, tWidth * j, tHeight * i, tWidth, tWidth);
         } else if (map[idx] === 0) {
-            ctx.drawImage(grass, tWidth * j, tHeight * i, 40, 40);
+            ctx.drawImage(grass, tWidth * j, tHeight * i, tWidth, tWidth);
         } else if (map[idx] === 2) {
-          ctx.drawImage(water, tWidth * j, tHeight * i, 40, 40);
+          ctx.drawImage(water, tWidth * j, tHeight * i, tWidth, tWidth);
         } else if (map[idx] === 3) {
-          ctx.drawImage(brick, tWidth * j, tHeight * i, 40, 40);
+          ctx.drawImage(brick, tWidth * j, tHeight * i, tWidth, tWidth);
         } else if (map[idx] === 4) {
           ctx.fillStyle = "black"
           ctx.fillRect(tWidth * j, tHeight * i, tWidth, tHeight)
         } else if (map[idx] === 5) {
-          ctx.drawImage(grass_water, tWidth * j, tHeight * i, 40, 40);
+          ctx.drawImage(grass_water, tWidth * j, tHeight * i, tWidth, tWidth);
         } else if (map[idx] === 6) {
-          ctx.drawImage(wood, tWidth * j, tHeight * i, 40, 40);
+          ctx.drawImage(wood, tWidth * j, tHeight * i, tWidth, tWidth);
         }
       }
     }
   }
 
   const printblock = () => {
-    let cur_x = Math.floor((xpos+20)/40)
-    let cur_y = Math.floor((ypos+20)/40)
+    let cur_x = Math.floor((xpos+(tWidth/2))/tWidth)
+    let cur_y = Math.floor((ypos+(tWidth/2))/tWidth)
     ctx.fillStyle = 'rgba(225,225,225,0.2)';
-    ctx.fillRect(tWidth * cur_x, tHeight * cur_y, 40,40);
+    ctx.fillRect(tWidth * cur_x, tHeight * cur_y, tWidth,tWidth);
   }
 
   const printchar = () => {
     switch(facing) {
       case "left":
-        ctx.drawImage(char_left, xpos, ypos, 40, 40);
+        ctx.drawImage(char_left, xpos, ypos, tWidth, tWidth);
         break;
       case "right":
-        ctx.drawImage(char_right, xpos, ypos, 40, 40);
+        ctx.drawImage(char_right, xpos, ypos, tWidth, tWidth);
         break;
       case "up":
-        ctx.drawImage(char_up, xpos, ypos, 40, 40);
+        ctx.drawImage(char_up, xpos, ypos, tWidth, tWidth);
         break;
       case "down":
-        ctx.drawImage(char_down, xpos, ypos, 40, 40);
+        ctx.drawImage(char_down, xpos, ypos, tWidth, tWidth);
         break;
       case "up_left":
-        ctx.drawImage(char_up_left, xpos, ypos, 40, 40);
+        ctx.drawImage(char_up_left, xpos, ypos, tWidth, tWidth);
         break;
       case "up_right":
-        ctx.drawImage(char_up_right, xpos, ypos, 40, 40);
+        ctx.drawImage(char_up_right, xpos, ypos, tWidth, tWidth);
         break;
       case "down_left":
-        ctx.drawImage(char_down_left, xpos, ypos, 40, 40);
+        ctx.drawImage(char_down_left, xpos, ypos, tWidth, tWidth);
         break;
       case "down_right":
-        ctx.drawImage(char_down_right, xpos, ypos, 40, 40);
+        ctx.drawImage(char_down_right, xpos, ypos, tWidth, tWidth);
         break;
     }
   }
