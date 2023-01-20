c5380da7b4f57eea104e7e5f75b9fb50c47daf55
kliu33 <kliu33@buffalo.edu>
2023-01-19 19:57:49 -0500

    background music works properly

--- src/scripts/audio.js
+++ src/scripts/audio.js
@@ -1,7 +1,5 @@
 function changeImage() {
     var imm = document.getElementById("audio")
-    var aud = document.getElementById('lofi')
-    aud.muted = !aud.muted;
     if (imm.getAttribute('src') == "./images/vol-on.png"){
         imm.src = './images/vol-off.png';
     } else {
