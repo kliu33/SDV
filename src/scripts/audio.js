bf39b2ae529780a5f1691d1dcece6b147b9995fc
kliu33 <kliu33@buffalo.edu>
2023-04-05 09:16:14 -0400

    fixed audio and bugs

--- src/scripts/audio.js
+++ src/scripts/audio.js
@@ -2,9 +2,15 @@ function changeImage() {
     var imm = document.getElementById("audio")
     var aud = document.getElementById('lofi')
     aud.muted = !aud.muted;
+    aud.play();
     if (imm.getAttribute('src') == "./images/vol-on.png"){
         imm.src = './images/vol-off.png';
     } else {
         imm.src = './images/vol-on.png';
     }
 }
+
+function changeVolume(vol) {
+    var aud = document.getElementById('lofi')
+    aud.volume = vol;
+  }
